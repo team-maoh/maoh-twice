b4d76273e4ccc95c6a73f576c2637b4134c78cbf
kmhanko <kmhankotiv@gmail.com>
2017-08-20 17:49:22 +0900

    no message

--- test_kmhanko/app/src/main/java/com/example/user/test_kmhanko/MainActivity.java
+++ test_kmhanko/app/src/main/java/com/example/user/test_kmhanko/MainActivity.java
@@ -3,11 +3,27 @@ package com.example.user.test_kmhanko;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    class TestSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+    }
+
+
 }
