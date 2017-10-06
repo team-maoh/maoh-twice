350ebb15c2f2d9ab0cd60ea08435a211ffb1c933
horie <horie@horieyuukinoMacBook-Pro.local>
2017-10-06 23:27:50 +0900

    cameoffsetのゲッター上げ直し

--- maohx2/app/src/main/java/com/maohx2/horie/map/MapAdmin.java
+++ maohx2/app/src/main/java/com/maohx2/horie/map/MapAdmin.java
@@ -75,6 +75,14 @@ public class MapAdmin {
         return map_size_y;
     }
 
+    public int getCameraOffset_x(){
+        return camera.getCameraOffset(1500,2000).x;
+    }
+
+    public int getCameraOffset_y(){
+        return camera.getCameraOffset(1500,2000).y;
+    }
+
 
 
     public void setMap_data(int i,int j,boolean isWall){
