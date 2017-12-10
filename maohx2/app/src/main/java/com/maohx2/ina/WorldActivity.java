6d531adeafbf2ebbf6c1656430d9f5057e0979a8
kmtiv <kmtiv@icloud.com>
2017-12-10 20:11:16 +0900

    WorldのTouchState

--- maohx2/app/src/main/java/com/maohx2/ina/WorldActivity.java
+++ maohx2/app/src/main/java/com/maohx2/ina/WorldActivity.java
@@ -66,13 +66,14 @@ class WorldSurfaceView extends BaseSurfaceView {
         graphic.bookingDrawBitmapName("キノコの森",300,590);
         world_game_system.draw();
 
+        /*
         if (touch_state == TouchState.DOWN) {
-             by kmhanko
             thread = null;
             Intent intent = new Intent(map_activity, DungeonActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
             map_activity.startActivity(intent);
 
         }
+        */
     }
 }
