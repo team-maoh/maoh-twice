3af876fde56e68209a3a26b871ecd7f0a1596987
DESKTOP-UEQ6TTH\ina <inayasu19950424@gmail.com>
2018-01-21 17:59:39 +0900

    ボタンをほかの人が一応使えるようにした。 （今後更新予定）

--- maohx2/app/src/main/java/com/maohx2/ina/StartActivity.java
+++ maohx2/app/src/main/java/com/maohx2/ina/StartActivity.java
@@ -9,6 +9,9 @@ import android.widget.RelativeLayout;
 
 import com.maohx2.ina.Draw.BitmapData;
 import com.maohx2.ina.Draw.Graphic;
+import com.maohx2.ina.Text.BoxTextPlate;
+import com.maohx2.ina.Text.CircleImagePlate;
+import com.maohx2.ina.Text.PlateGroup;
 import com.maohx2.ina.UI.UserInterface;
 import com.maohx2.kmhanko.database.MyDatabaseAdmin;
 
@@ -63,6 +66,11 @@ class StartSurfaceView extends BaseSurfaceView {
     StartGameSystem start_game_system;
     UserInterface start_user_interface;
 
+    BoxTextPlate[] box_text_plates = new BoxTextPlate[3];
+    PlateGroup<BoxTextPlate> item_list;
+
+    CircleImagePlate[] circle_image_plate = new CircleImagePlate[1];
+    PlateGroup<CircleImagePlate> image_list;
 
     public StartSurfaceView(Activity _start_activity) {
         super(_start_activity);
@@ -80,18 +88,59 @@ class StartSurfaceView extends BaseSurfaceView {
         my_database_admin.addMyDatabase("StartDB", "LocalStartImage.db", 1, "r");
         graphic.loadLocalImages(my_database_admin.getMyDatabase("StartDB"), "Start");
 
-        paint.setColor(Color.rgb(100,100,0));
+        paint.setColor(Color.rgb(100, 100, 0));
         paint.setTextSize(30);
         srime = graphic.searchBitmap("スライム");
-        bit_srime = graphic.processTrimmingBitmapData(srime,0,0,46,46);
+        bit_srime = graphic.processTrimmingBitmapData(srime, 0, 0, 46, 46);
 
         start_user_interface = new UserInterface(global_data.getGlobalConstants(), graphic);
         start_user_interface.init();
 
         start_game_system = new StartGameSystem();
-        start_game_system.init(holder, graphic,  start_user_interface, start_activity, my_database_admin);
+        start_game_system.init(holder, graphic, start_user_interface, start_activity, my_database_admin);
+
+        int[][] position = new int[3][4];
+        String[] text = new String[3];
+
+        position[0][0] = 800;
+        position[0][1] = 100;
+        position[0][2] = 900;
+        position[0][3] = 200;
+
+        position[1][0] = 200;
+        position[1][1] = 750;
+        position[1][2] = 400;
+        position[1][3] = 850;
+
+        position[2][0] = 1000;
+        position[2][1] = 750;
+        position[2][2] = 1600;
+        position[2][3] = 850;
+
+        text[0] = "祝";
+        text[1] = "ご";
+        text[2] = "き";
 
 
+        for (int i = 0; i < 3; i++) {
+            box_text_plates[i] = new BoxTextPlate(graphic, start_user_interface, paint, Constants.Touch.TouchWay.UP_MOMENT, Constants.Touch.TouchWay.MOVE, position[i], text[i], paint);
+        }
+
+        item_list = new PlateGroup<BoxTextPlate>(box_text_plates);
+
+
+
+
+
+        int[] circle_position = new int[3];
+
+        circle_position[0] = 800;
+        circle_position[1] = 450;
+        circle_position[2] = 100;
+
+        circle_image_plate[0] = new CircleImagePlate(graphic, start_user_interface, paint, Constants.Touch.TouchWay.UP_MOMENT, Constants.Touch.TouchWay.MOVE, circle_position, graphic.searchBitmap("スライム"));
+
+        image_list = new PlateGroup<CircleImagePlate>(circle_image_plate);
 
 
 
@@ -110,8 +159,26 @@ class StartSurfaceView extends BaseSurfaceView {
 
         //graphic.bookingDrawBitmapData(graphic.processBitmapData(test_bitmap_data,1,4,255),100,100);
 
+        /*
+        if(touch_state == TouchState.DOWN){
+            thread = null;
+            Intent intent = new Intent(start_activity, WorldActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            start_activity.startActivity(intent);
+        }
+*/
+
+        start_user_interface.updateTouchState(touch_x, touch_y, touch_state);
+        start_game_system.updata();
+        item_list.updaate();
+        image_list.updaate();
+
 
         graphic.bookingDrawBitmapName("スライム",300,590);
+        item_list.draw();
+        image_list.draw();
+        start_game_system.draw();
+
         /*
         graphic.bookingDrawText("(300,590)",300,590,paint);
         graphic.bookingDrawBitmapData(bit_srime,640,100);
@@ -145,18 +212,6 @@ class StartSurfaceView extends BaseSurfaceView {
         graphic.bookingDrawText("1500",1500,100,paint);
         graphic.bookingDrawText("1600",1600,100,paint);
 */
-        start_user_interface.updateTouchState(touch_x, touch_y, touch_state);
-
-        start_game_system.updata();
-        start_game_system.draw();
-
-
-        if(touch_state == TouchState.DOWN){
-            thread = null;
-            Intent intent = new Intent(start_activity, WorldActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-            start_activity.startActivity(intent);
-        }
 
     }
 }
--- maohx2/app/src/main/java/com/maohx2/ina/Text/CircleImagePlate.java
+++ maohx2/app/src/main/java/com/maohx2/ina/Text/CircleImagePlate.java
@@ -5,17 +5,18 @@ import android.graphics.Paint;
 import com.maohx2.ina.Constants.Touch.TouchWay;
 import com.maohx2.ina.Draw.BitmapData;
 import com.maohx2.ina.Draw.Graphic;
+import com.maohx2.ina.UI.UserInterface;
 
 /**
  * Created by ina on 2018/01/14.
  */
 
-public class CircleImageButton extends CircleButton {
+public class CircleImagePlate extends CirclePlate {
 
     BitmapData button_image;
 
-    CircleImageButton(Graphic _graphic, Paint _paint, TouchWay _judge_way, TouchWay _feedback_way, int[] position, BitmapData _button_image) {
-        super(_graphic, _paint, _judge_way, _feedback_way, position[0], position[1], position[2]);
+    public CircleImagePlate(Graphic _graphic, UserInterface _user_interface, Paint _paint, TouchWay _judge_way, TouchWay _feedback_way, int[] position, BitmapData _button_image) {
+        super(_graphic, _user_interface, _paint, _judge_way, _feedback_way, position[0], position[1], position[2]);
 
         button_image = _button_image;
     }
