666f8fa7d7ced95218525fc29986791057b67279
DESKTOP-UEQ6TTH\ina <inayasu19950424@gmail.com>
2018-03-30 20:50:06 +0900

    武器データ読み込み追加。

--- maohx2/app/src/main/java/com/maohx2/ina/ItemData/EquipmentItemDataAdmin.java
+++ maohx2/app/src/main/java/com/maohx2/ina/ItemData/EquipmentItemDataAdmin.java
@@ -18,7 +18,8 @@ public class EquipmentItemDataAdmin extends ItemDataAdmin<EquipmentItemData> {
         super(_graphic, _database_admin);
         itoe = EQUIPMENT_KIND.values();
 
-        loadItemData("EquipmentItemData");
+        //loadItemData("EquipmentItemData");
+        loadItemData("EquipmentItemDebugData");
     }
 
     @Override
@@ -26,20 +27,34 @@ public class EquipmentItemDataAdmin extends ItemDataAdmin<EquipmentItemData> {
         int size = database.getSize(tableName);
 
         List<String> name = database.getString(tableName, "name");
-        List<String> image_name = database.getString(tableName, "image_name");
-        //List<Integer> item_kind = database.getInt(tableName, "item_kind");
-        List<Integer> equipment_kind = database.getInt(tableName, "equipment_kind");
+        List<String> imageName = database.getString(tableName, "imageName");
         List<Integer> price = database.getInt(tableName, "price");
+        List<Integer> equipmentKind = database.getInt(tableName, "equipmentKind");
+        List<Integer> useNum = database.getInt(tableName, "useNum");
+        List<Integer> radius = database.getInt(tableName, "radius");
+        List<Float> decayRate = database.getFloat(tableName, "decayRate");
+        List<Integer> touchFrequency = database.getInt(tableName, "touchFrequency");
+        List<Float> autoFrequencyRate = database.getFloat(tableName, "autoFrequencyRate");
+        List<Integer> attack = database.getInt(tableName, "attack");
+        List<Integer> defence = database.getInt(tableName, "defence");
 
 
         for (int i = 0; i < size; i++) {
             datas.add(new EquipmentItemData());
-            datas.get(i).setName(name.get(i));
-            datas.get(i).setImageName(image_name.get(i));
-            datas.get(i).setEquipmentKind(itoe[equipment_kind.get(i)]);
-            datas.get(i).setPrice(price.get(i));
             datas.get(i).setItemKind(ITEM_KIND.EXPEND);
-            datas.get(i).setItemImage(graphic.searchBitmap(image_name.get(i)));
+            datas.get(i).setName(name.get(i));
+            datas.get(i).setImageName(imageName.get(i));
+            datas.get(i).setItemImage(graphic.searchBitmap(imageName.get(i)));
+            datas.get(i).setPrice(price.get(i));
+            datas.get(i).setEquipmentKind(itoe[equipmentKind.get(i)]);
+            datas.get(i).setUseNum(useNum.get(i));
+            datas.get(i).setRadius(radius.get(i));
+            datas.get(i).setDecayRate(decayRate.get(i));
+            datas.get(i).setTouchFrequency(touchFrequency.get(i));
+            datas.get(i).setAutoFrequencyRate(autoFrequencyRate.get(i));
+            datas.get(i).setAttack(attack.get(i));
+            datas.get(i).setDefence(defence.get(i));
+
         }
     }
 
