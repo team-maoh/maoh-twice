a8cf185bf49f835eed4216812f55a1c26994d8a0
kmtiv <kmtiv@icloud.com>
2018-06-05 11:45:32 +0900

    採掘画面背景の黒

--- maohx2/app/src/main/assets/database/backgroundBlack.sh
+++ maohx2/app/src/main/assets/database/backgroundBlack.sh
@@ -18,7 +18,7 @@ parentDirName=`basename $parentDir`
 for backgroundFile in `find $parentDir -mindepth 3 -maxdepth 3 -type f -name "old_firstBackground.*"`; do
 
   #oldBackgroundFile=${backgroundFile%/*}/old_firstBackground.${backgroundFile##*.}
-  newBackgroundFile=${backgroundFile%/*}/firstBackground.png
+  newBackgroundFile=${backgroundFile%/*}/miningBack.png
   #${backgroundFile##*.}
 
   #echo $oldBackgroundFile
@@ -29,5 +29,6 @@ for backgroundFile in `find $parentDir -mindepth 3 -maxdepth 3 -type f -name "ol
   #なんか同時に丸を複数かけないぽい？
   #convert $backgroundFile -fill "#000000" -draw "circle $circle1PosX $circle1PosY $circle1PosX $((${circle1PosY} + ${circle1R}))" -fill "#000000" -draw "circle $circle2PosX $circle2PosY $circle2PosX $((${circle2PosY} + ${circle2R}))" $newBackgroundFile
   convert $backgroundFile -fill "#000000" -draw "rectangle $boxX1 $boxY1 $boxX2 $boxY2" -fill "#000000" -draw "circle $circle1PosX $circle1PosY $circle1PosX $((${circle1PosY} + ${circle1R}))" -fill "#000000" -draw "circle $circle2PosX $circle2PosY $circle2PosX $((${circle2PosY} + ${circle2R}))" $newBackgroundFile
+  #convert $backgroundFile -fill "#000000" -draw "rectangle $boxX1 $boxY1 $boxX2 $boxY2" -fill "#000000" -draw "circle $circle1PosX $circle1PosY $circle1PosX $((${circle1PosY} + ${circle1R}))" $newBackgroundFile
 
 done
