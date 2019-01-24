40cdc422d028bf3de1b3a4f62253aae8513f66a2
kmtiv <kmtiv@icloud.com>
2019-01-24 21:25:50 +0900

    DEMOモード変数をコンスタントに追加

--- maohx2/app/src/main/java/com/maohx2/ina/Constants.java
+++ maohx2/app/src/main/java/com/maohx2/ina/Constants.java
@@ -54,6 +54,14 @@ public final class Constants {
     public static final int STAGE_NUM = 7;
 
     public static final String DEBUG_SAVE_MODE = "s";
+
+    public static class DEMO {
+        public enum DEMO_MODE {
+            OFF,
+            ON
+        }
+    }
+
     public static class SaveDataVersion {
         public static final int EQUIPMENT_INVENTRY = 1;
         public static final int EXPEND_INVENTRY = 1;
